759a80cb1af363f68188de036b475c1a88c1321b
SorokoNV <soroko_nikolai@mail.ru>
2024-11-11 18:04:45 +0300

    В код class Main добавлина логика поиска фрагмента программы и подсчет доли запросов

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,14 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         int fileNumber = 0;
+        String program1="YandexBot";
+        String program2="Googlebot";
         do {
             System.out.println("Введите путь к файлу и нажмите Enter");
             String path = new Scanner(System.in).nextLine();
@@ -24,8 +28,8 @@ public class Main {
             fileNumber = fileNumber + 1;
             System.out.println("Это файл номер " + fileNumber);
             int cuntLine =0;
-            int maxLength =0;
-            int minLength =0;
+            int countRequestsYandexBot=0;
+            int countRequestsGooglebot=0;
             try {
                 FileReader fileReader = new FileReader(path);
                 BufferedReader reader =
@@ -34,25 +38,40 @@ public class Main {
                 while ((line = reader.readLine()) != null) {
                     int length = line.length();
                     cuntLine = cuntLine +1;
-                    if (length > 1024)
-                        throw new RuntimeException("\n" +
-                                "The number of characters in a line №"+cuntLine+" exceeds 1024");
-                    if (minLength==0)
-                        minLength=length;
-                    if (minLength>length)
-                        minLength=length;
-                    if (length>maxLength)
-                        maxLength=length;
+                    String regex = "\\s(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
+                    String[] parts = line.split(regex);
+                    String userAgent = parts[9];
+                    if (searchForProgramThatMakesQueries(userAgent,program1))
+                        countRequestsYandexBot=countRequestsYandexBot+1;
+                    if (searchForProgramThatMakesQueries(userAgent,program2))
+                        countRequestsGooglebot=countRequestsGooglebot+1;
                 }
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
             System.out.println("Общее количество строк в файле "+cuntLine);
-            System.out.println("Длина самой длинной строки в файле "+maxLength);
-            System.out.println("Длина самой короткой строки в файле "+minLength);
+            System.out.println("Число запросов "+ program1+" равно "+countRequestsYandexBot);
+            System.out.println("Число запросов "+ program2+" равно "+countRequestsGooglebot);
+            System.out.println("Доля запросов "+ program1+" равна "+shareOfRequestsProgram(cuntLine,countRequestsYandexBot));
+            System.out.println("Доля запросов "+ program2+" равна "+shareOfRequestsProgram(cuntLine,countRequestsGooglebot));
         }
         while (true);
 
     }
-
+    public static boolean searchForProgramThatMakesQueries (String userAgent, String program) {
+        String regex = "compatible;\\s*(\\S+)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(userAgent);
+        if (matcher.find()) {
+            String fragment = matcher.group(1);
+            String[] fragments = fragment.split("/");
+            if (fragments[0].equals(program))
+                return true;
+        }
+        return false;
+    }
+    public static double shareOfRequestsProgram (int allRequests, int requestsProgram){
+        double shareRequests =(double) requestsProgram /  allRequests;
+        return shareRequests;
+    }
 }
